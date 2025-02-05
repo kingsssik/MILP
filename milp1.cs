b217c5fc8732519b7e57600ebe1a1a1ab1815f01
kingsssik <141576181+kingsssik@users.noreply.github.com>
2025-02-05 13:38:29 +0900

    Update milp1.cs

--- milp1.cs
+++ milp1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel;
 using System.Numerics;
 using System.Runtime.ExceptionServices;
@@ -19,7 +19,7 @@ class Milp
             double M = 999999; //큰 양수
 
 
-            int[] r = new int[] { 3, 2, 2, 3 }; //
+            int[] r = new int[] { 0, 3, 2, 2, 3 }; //
             int[] d = new int[] { 20, 15, 25, 30 }; //
 
             Cplex cplex = new Cplex();
@@ -30,19 +30,23 @@ class Milp
 
             //decision variable
 
-            IIntVar[][][][][] X_jlhzi = new IIntVar[n][][][][];
-            for(int j = 0;  j < n; j++)
+            IIntVar[][][][][] X_jlhzi = new IIntVar[n + 1][][][][];
+            for (int j = 1; j < n + 1; j++)
             {
-                X_jlhzi[j] = new IIntVar[r[j]][][][];
-                for(int l = 0; l < r[j]; l++)
+                X_jlhzi[j] = new IIntVar[r[j] + 1][][][];
+                for (int l = 1; l < r[j] + 1; l++)
                 {
-                    X_jlhzi[j][l] = new IIntVar[n+1][][];
-                    for(int h =0; h < n+1; h++)
+                    X_jlhzi[j][l] = new IIntVar[n + 1][][];
+                    for (int h = 0; h < n + 1; h++)
                     {
-                        X_jlhzi[j][l][h] = new IIntVar[r[j]][];
-                        for(int z = 0; z < r[j]; z++)
+                        X_jlhzi[j][l][h] = new IIntVar[r[j] + 1][];
+                        for (int z = 1; z < r[j] + 1; z++)
                         {
-                            X_jlhzi[j][l][h][z] = new IIntVar[m];
+                            X_jlhzi[j][l][h][z] = new IIntVar[m + 1];
+                            for (int i = 0; i < m + 1; i++)
+                            {
+                                X_jlhzi[j][l][h][z][i] = cplex.IntVar(0, 1, $"X_{j}_{l}_{h}_{z}_{i}");
+                            }
                         }
                     }
                 }
@@ -50,27 +54,27 @@ class Milp
 
 
 
-            IIntVar[][] C_jl = new IIntVar[n][];
-            for(int j = 0; j < n; j++)
+            IIntVar[][] C_jl = new IIntVar[n + 1][];
+            for (int j = 1; j < n + 1; j++)
             {
-                C_jl[j] = new IIntVar[r[j]];
+                C_jl[j] = new IIntVar[r[j] + 1];
             }
 
 
 
-            IIntVar[] T_j = new IIntVar[n];
+            IIntVar[] T_j = new IIntVar[n+1];
 
 
             //intialization
 
-            double[][][] p_jli = new double[n][][];
-            for (int j = 0; j < n; j++)
+            double[][][] p_jli = new double[n + 1][][];
+            for (int j = 1; j < n + 1; j++)
             {
-                p_jli[j] = new double[r[j]][];
-                for (int l = 0; l < r[j]; l++)
+                p_jli[j] = new double[r[j] + 1][];
+                for (int l = 1; l < r[j] + 1; l++)
                 {
-                    p_jli[j][l] = new double[m];
-                    for (int i = 0; i < m; i++)
+                    p_jli[j][l] = new double[m + 1];
+                    for (int i = 1; i < m + 1; i++)
                     {
                         p_jli[j][l][i] = (Convert.ToDouble(ran.Next(1000)) / 1000) * 10;
                     }
@@ -79,14 +83,14 @@ class Milp
 
 
 
-            double[][][] e_jli = new double[n][][];
-            for (int j = 0; j < n; j++)
+            double[][][] e_jli = new double[n + 1][][];
+            for (int j = 1; j < n + 1; j++)
             {
-                e_jli[j] = new double[r[j]][];
-                for (int l = 0; l < r[j]; l++)
+                e_jli[j] = new double[r[j] + 1][];
+                for (int l = 1; l < r[j] + 1; l++)
                 {
-                    e_jli[j][l] = new double[m];
-                    for (int i = 0; i < m; i++)
+                    e_jli[j][l] = new double[m + 1];
+                    for (int i = 1; i < m + 1; i++)
                     {
                         double a = (Convert.ToDouble(ran.Next(1000)) / 1000) * 1;
 
@@ -103,55 +107,121 @@ class Milp
                 }
             }
 
-            double[][][] S_jhi = new double[n][][];
-            for(int j = 0; j < n; j++)
+            double[][][] S_jhi = new double[n + 1][][];
+            for (int j = 1; j < n + 1; j++)
             {
-                S_jhi[j] = new double[n+1][];
-                for (int h = 0; h < n+1; h++)
+                S_jhi[j] = new double[n + 1][];
+                for (int h = 0; h < n + 1; h++)
                 {
-                    S_jhi[j][h] = new double[m];
-                    for(int i = 0;i < m; i++)
+                    S_jhi[j][h] = new double[m + 1];
+                    for (int i = 1; i < m + 1; i++)
                     {
                         S_jhi[j][h][i] = (Convert.ToDouble(ran.Next(1000)) / 1000) * 6;
                     }
                 }
             }
 
+            //objective
+            INumExpr sumT = cplex.NumExpr();
+            for (int j = 1; j < n+1; j++)
+            {
+                sumT = cplex.Sum(sumT, T_j[j]);
+            }
+            //cplex.AddMinimize(sumT);
 
             //constraint
 
             //(1)
-           
-            for (int j = 0;j < n; j++)
+
+            for (int j = 1; j < n + 1; j++)
             {
-                for(int l = 0; l < r[j]; l++)
+                for (int l = 1; l < r[j] + 1; l++)
                 {
                     INumExpr sumX = cplex.NumExpr();
-                    for (int h = 0; h < n+1; h++)
+                    for (int h = 0; h < n + 1; h++)
                     {
-                        for(int z = 0; z < m; z++)
+                        for (int z = 1; z < r[h] + 1; z++)
                         {
-                            for(int i = 0; i < m; i++)
+                            for (int i = 1; i < m + 1; i++)
                             {
                                 sumX = cplex.Sum(sumX, X_jlhzi[j][l][h][z][i]);
                             }
                         }
                     }
+                    cplex.AddEq(sumX, 1.0);
                 }
             }
 
- 
+
+
+            //(2)
+
+            for (int j = 1; j < n + 1; j++)
+            {
+                for (int l = 1; l < r[j] + 1; l++)
+                {
+                    for (int i = 1; i < m + 1; i++)
+                    {
+                        INumExpr sumX_1 = cplex.NumExpr();
+
+                        for (int h = 0; h < n + 1; h++)
+                        {
+                            for (int z = 1; z < r[h] + 1; z++)
+                            {
+                                sumX_1 = cplex.Sum(sumX_1, X_jlhzi[j][l][h][z][i]);
+                            }
+                        }
+                        cplex.AddLe(sumX_1, e_jli[j][l][i]);
+                    }
+                }
+            }
+
+            //(3)
+
+            for (int h = 1; h < n + 1; h++)
+            { 
+                for (int z = 1; z < r[h]+1; z++)
+                {
+                    INumExpr sumX_2 = cplex.NumExpr();
+                    for (int j = 1; j <n+1; j++)
+                    {
+                        for(int l =1; l < r[j]+1; l++)
+                        {
+                            for(int i = 1; i<n+1; i++)
+                            {
+                                sumX_2 = cplex.Sum(sumX_2,X_jlhzi[j][l][h][z][i]);
+                            }
+                        }
+                    }
+                    cplex.AddLe(sumX_2, 1.0);
+                }
+            }
+
+            //(4)
+
+            for(int i = 0; i<m+1; i++)
+            {
+                INumExpr sumX_3 = cplex.NumExpr();
+                for(int j = 1;j <n+1; j++)
+                {
+                    for(int l =1; l < r[j]+1; l++)
+                    {
+                        sumX_3 = cplex.Sum(sumX_3, X_jlhzi[j][l][0][1][i]);
+                    }
+                }
+                cplex.AddLe(sumX_3,1.0);
+            }
+
+            //(5)
+
+
+
+
 
 
 
 
         }
-
-
-
-
-
-
         catch (ILOG.Concert.Exception exc)
         {
             System.Console.WriteLine("Concert exception '" + exc + "' caught");
